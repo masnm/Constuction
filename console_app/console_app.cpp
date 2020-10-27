e58193d80334381551ed813c5acaf5ec4c6d9428
A.S. Noman <nomanahmmed0@gmail.com>
2020-10-27 15:16:19 +0600

    last commit

--- console_app/console_app.cpp
+++ console_app/console_app.cpp
@@ -8,50 +8,77 @@
 
 #include <Windows.h>
 
+
+template<typename type>
 struct i2d {
-	int x;
-	int y;
-
+	type x;
+	type y;
+	i2d() : x(0), y(0) {}
+	i2d(type _x, type _y) : x(_x), y(_y) {}
+	i2d operator + (const i2d& right) const { return i2d(this->x + right.x, this->y + right.y); }
+	i2d operator - (const i2d& right) const { return i2d(this->x - right.x, this->y - right.y); }
+	i2d operator / (const int right) const { return i2d(this->x / right, this - y / right); }
+	i2d& operator += (const i2d& right) { this->x += right.x; this->y += right.y; return *this; }
 };
+typedef i2d<int> int_2d;
 
-int nScreenWidth = 60;
-int nScreenHeight = 30;
+int_2d screen_dimantion = { 60, 30 };
 
 int main()
 {
 	// Creating Screen Buffer
-	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	wchar_t* screen = new wchar_t[screen_dimantion.x*screen_dimantion.y];
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
-	//SetConsoleMode(hConsole, ENABLE_WRAP_AT_EOL_OUTPUT | DISABLE_NEWLINE_AUTO_RETURN );
-
-	COORD coaard; coaard.X = nScreenWidth; coaard.Y = nScreenHeight;
-	SMALL_RECT rect; rect.Top = 0; rect.Left = 0; rect.Bottom = nScreenHeight - 1; rect.Right = nScreenWidth - 1;
+	COORD coaard; coaard.X = screen_dimantion.x; coaard.Y = screen_dimantion.y;
+	SMALL_RECT rect; rect.Top = 0; rect.Left = 0; rect.Bottom = screen_dimantion.y - 1; rect.Right = screen_dimantion.x - 1;
 	SetConsoleWindowInfo(hConsole, TRUE, &rect);
 	SetConsoleScreenBufferSize(hConsole, coaard);
 
+	// staring from here
+	std::vector<int_2d> snake;
+	int_2d xone = { 1,0 }, yone = { 0,1 };
+	snake.push_back({ screen_dimantion.x / 2, screen_dimantion.y / 2 });
+	for (int i = 1; i < 6; i++) { snake.push_back(snake[i - 1] - xone); }
 
-	CONSOLE_SCREEN_BUFFER_INFO bf;
-	GetConsoleScreenBufferInfo(hConsole, &bf);
+	int left = 0, right = 2, up = 1, down = 3, dir = down;
 
-
-	// clearing screen // or filling array winth 'space'
-	//for (int i = 0; i < nScreenWidth * nScreenHeight; i++) { screen[i] = L' '; }
 
 	// main game loop
 	while (true) {
 
-		screen[nScreenWidth * nScreenHeight - 1] = L'E';
-		screen[nScreenWidth-1] = L'W';
-		screen[0] = L'S';
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+		if (dir == left) {
+			snake.erase(snake.begin());
+			snake.push_back(snake[snake.size() - 1] + xone);
+			if (snake[snake.size() - 1].x >= screen_dimantion.x) snake[snake.size() - 1].x = 0;
+		} else if (dir == right) {
+			snake.erase(snake.begin());
+			snake.push_back(snake[snake.size() - 1] - xone);
+			if (snake[snake.size() - 1].x < 0) snake[snake.size() - 1].x = screen_dimantion.x-1;
+		} else if (dir == up) {
+			snake.erase(snake.begin());
+			snake.push_back(snake[snake.size() - 1] - yone);
+			if (snake[snake.size() - 1].y < 0) snake[snake.size() - 1].y = screen_dimantion.y - 1;
+		} else if (dir == down) {
+			snake.erase(snake.begin());
+			snake.push_back(snake[snake.size() - 1] + yone);
+			if (snake[snake.size() - 1].y >= screen_dimantion.y) snake[snake.size() - 1].y = 0;
+		}
+
+		// drawing snake
+		for (auto i : snake) {
+			screen[i.y * screen_dimantion.x + i.x] = L'#';
+		}
 
 		// Presenting frame
-		//screen[nScreenWidth * nScreenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
-		// cleaning screen
-		for (int i = 0; i < nScreenWidth * nScreenHeight; i++) { screen[i] = L' '; }
+		WriteConsoleOutputCharacter(hConsole, screen, screen_dimantion.x * screen_dimantion.y, { 0,0 }, &dwBytesWritten);
+
+		// cleaning screen array
+		for (int i = 0; i < screen_dimantion.x * screen_dimantion.y; i++) { screen[i] = L' '; }
 	}
 
 	//system("pause");
